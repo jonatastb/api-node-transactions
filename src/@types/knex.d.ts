a0863204705d780059bfefad8ca05fa8d2d228a5
jonatastb <jonatas.bueno@outlook.com>
2024-04-02 20:53:43 -0300

    feat: update repo

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line
 import knex from 'knex'
 
-export interface Tables {
-  transactions: {
-    id: string
-    title: string
-    amount: number
-    created_at: string
-    session_id?: string
+declare module 'knex/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string
+      title: string
+      amount: number
+      created_at: string
+      session_id?: string
+    }
   }
 }
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,44 +1,109 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { knex } from '../database'
-import crypto from 'node:crypto'
+import crypto, { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
-
-    return { transactions }
+  app.addHook('preHandler', async (request, reply) => {
+    console.log(`[${request.method}] ${request.url}`)
   })
 
-  app.get('/:id', async (request) => {
-    const getTransactionParamsSchema = z.object({
-      id: z.string().uuid(),
-    })
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
 
-    const { id } = getTransactionParamsSchema.parse(request.params)
+      const transactions = await knex('transactions')
+        .where('sessionId', sessionId)
+        .select()
 
-    const transaction = await knex('transactions').where('id', id).first()
+      return { transactions }
+    },
+  )
 
-    return { transaction }
-  })
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionsParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
 
-  app.post('/', async (request, reply) => {
-    const createTransactionBodySchema = z.object({
-      title: z.string(),
-      amount: z.number(),
-      type: z.enum(['credit', 'debit']),
-    })
+      const { sessionId } = request.cookies
 
-    const { title, amount, type } = createTransactionBodySchema.parse(
-      request.body,
-    )
+      const { id } = getTransactionsParamsSchema.parse(request.params)
 
-    await knex('transactions').insert({
-      id: crypto.randomUUID(),
-      title,
-      amount: type === 'credit' ? amount : amount * -1,
-    })
+      const transaction = await knex('transactions')
+        .where({
+          sessionId,
+          id,
+        })
+        .first()
 
-    return reply.status(201).send()
-  })
+      return {
+        transaction,
+      }
+    },
+  )
+
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const summary = await knex('transactions')
+        .sum('amount', { as: 'amount' })
+        .where({ sessionId })
+        .first()
+
+      return { summary }
+    },
+  )
+
+  app.post(
+    '/',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const createTransactionBodySchema = z.object({
+        title: z.string(),
+        amount: z.number(),
+        type: z.enum(['credit', 'debit']),
+      })
+
+      const { title, amount, type } = createTransactionBodySchema.parse(
+        request.body,
+      )
+
+      let sessionId = request.cookies.sessionId
+
+      if (!sessionId) {
+        sessionId = randomUUID()
+
+        reply.cookie('sessionId', sessionId, {
+          path: '/',
+          maxAge: 60 * 60 * 24 * 7, // 7 days
+        })
+      }
+
+      await knex('transactions').insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type === 'credit' ? amount : amount * -1,
+        sessionId,
+      })
+
+      return reply.status(201).send()
+    },
+  )
 }
